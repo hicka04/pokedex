3b6e5342e570324998724903b7798956682229ac
hicka04 <hicka04@gmail.com>
2023-09-16 11:36:54 +0900

    Extract targetSdk to version catalog

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     defaultConfig {
         applicationId = "dev.hicka04.pokedex.android"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = libs.versions.sdk.target.get().toInt()
         versionCode = 1
         versionName = "1.0"
     }
