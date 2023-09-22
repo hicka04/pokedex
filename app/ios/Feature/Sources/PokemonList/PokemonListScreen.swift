15535ec26f8791d51b6a9538cc33439e1b51e225
hicka04 <hicka04@gmail.com>
2023-09-22 23:08:36 +0900

    Add PokemonListViewModel for iOS

--- app/ios/Feature/Sources/PokemonList/PokemonListScreen.swift
+++ app/ios/Feature/Sources/PokemonList/PokemonListScreen.swift
@@ -1,25 +1,52 @@
 import SwiftUI
 import shared
 
+@MainActor
 public struct PokemonListScreen: View {
-    @State var pokemonList: [Pokemon] = []
-    let getPokemonListUseCase = GetPokemonListUseCase()
+    let viewModel = PokemonListViewModel()
 
     public init() {}
 
     public var body: some View {
-        List(pokemonList) { pokemon in
-            Text(pokemon.name)
-        }.task {
-            do {
-                pokemonList = try await getPokemonListUseCase()
-            } catch {}
+        ContentView(
+            state: viewModel.state,
+            onAppear: viewModel.onAppear
+        )
+    }
+}
+
+extension PokemonListScreen {
+    @MainActor
+    final class State: ObservableObject {
+        @Published var pokemonList: [Pokemon]
+
+        init(pokemonList: [Pokemon] = []) {
+            self.pokemonList = pokemonList
+        }
+    }
+
+    struct ContentView: View {
+        @StateObject var state: State
+        var onAppear: @Sendable () async -> Void = {}
+
+        var body: some View {
+            List(state.pokemonList) { pokemon in
+                Text(pokemon.name)
+            }.task(onAppear)
         }
     }
 }
 
-struct SwiftUIView_Previews: PreviewProvider {
+struct PokemonListScreen_Previews: PreviewProvider {
     static var previews: some View {
-        PokemonListScreen()
+        PokemonListScreen.ContentView(
+            state: .init(
+                pokemonList: [
+                    .init(id: 1, name: "Bulbasaur"),
+                    .init(id: 2, name: "Ivysaur"),
+                    .init(id: 3, name: "Venusaur"),
+                ]
+            )
+        )
     }
 }
---
+++ app/ios/Feature/Sources/PokemonList/PokemonListViewModel.swift
@@ -0,0 +1,20 @@
+import Foundation
+import shared
+
+@MainActor
+final class PokemonListViewModel {
+    let state = PokemonListScreen.State()
+    let getPokemonListUseCase: GetPokemonListUseCase
+
+    init(getPokemonListUseCase: GetPokemonListUseCase = .init()) {
+        self.getPokemonListUseCase = getPokemonListUseCase
+    }
+
+    @Sendable func onAppear() async {
+        do {
+            state.pokemonList = try await getPokemonListUseCase()
+        } catch {
+            // TODO: handle error
+        }
+    }
+}
