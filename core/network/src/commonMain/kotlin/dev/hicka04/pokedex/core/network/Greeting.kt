9ea3052970c15a790f7ed0df1643c89d2e782ff6
hicka04 <hicka04@gmail.com>
2024-09-22 23:21:55 +0900

    Remove unnecessary files

--- core/network/src/commonMain/kotlin/dev/hicka04/pokedex/core/network/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package dev.hicka04.pokedex.core.network
-
-class Greeting {
-    private val platform: Platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
