d887c4ff92c8fa446c95c027da85a2dd5e3e8a88
hicka04 <hicka04@gmail.com>
2024-10-05 23:39:11 +0900

    Remove unused files

--- core/data/src/androidMain/kotlin/dev/hicka04/pokedex/core/data/Platform.android.kt
+++
@@ -1,7 +0,0 @@
-package dev.hicka04.pokedex.core.data
-
-class AndroidPlatform : Platform {
-    override val name: String = "Android ${android.os.Build.VERSION.SDK_INT}"
-}
-
-actual fun getPlatform(): Platform = AndroidPlatform()
--- core/data/src/commonMain/kotlin/dev/hicka04/pokedex/core/data/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package dev.hicka04.pokedex.core.data
-
-class Greeting {
-    private val platform: Platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
